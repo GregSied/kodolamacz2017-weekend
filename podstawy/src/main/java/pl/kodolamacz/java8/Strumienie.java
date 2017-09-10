8f8a5439fffb62cd437ebca11253376c60b6d2a3
Bartłomiej Tartanus <bartektartanus@gmail.com>
2017-09-10 12:06:17 +0200

    kolekcje

--- podstawy/src/main/java/pl/kodolamacz/java8/Strumienie.java
+++ podstawy/src/main/java/pl/kodolamacz/java8/Strumienie.java
@@ -1,9 +1,22 @@
 package pl.kodolamacz.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Strumienie {
 
     public static void main(String[] args) {
+        List<String> list = Arrays.asList("Frania",
+                "Ania","Henio", "Trusia", "Kasia", "Ala",
+                "Franciszek", "Wojciech");
 
+        List<String> collect = list.stream()
+                .filter(s -> s.startsWith("A"))
+                .collect(Collectors.toList());
+
+        list.forEach(System.out::println);
     }
 
 }
