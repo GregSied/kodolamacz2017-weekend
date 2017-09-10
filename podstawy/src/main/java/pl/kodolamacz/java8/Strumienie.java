03885833614b76416e20f0d622b10a4afe7de487
Bartłomiej Tartanus <bartektartanus@gmail.com>
2017-09-10 12:15:34 +0200

    strumienie

--- podstawy/src/main/java/pl/kodolamacz/java8/Strumienie.java
+++ podstawy/src/main/java/pl/kodolamacz/java8/Strumienie.java
@@ -1,9 +1,9 @@
 package pl.kodolamacz.java8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Strumienie {
 
@@ -12,11 +12,37 @@ public class Strumienie {
                 "Ania","Henio", "Trusia", "Kasia", "Ala",
                 "Franciszek", "Wojciech");
 
+        // ten kod
         List<String> collect = list.stream()
                 .filter(s -> s.startsWith("A"))
                 .collect(Collectors.toList());
 
-        list.forEach(System.out::println);
+        // jest równoważny temu:
+        List<String> collect2 = new ArrayList<>();
+        for (String s : list) {
+            if(s.startsWith("A")){
+                collect2.add(s);
+            }
+        }
+
+        list.stream()
+                .filter(s -> s.startsWith("A"))
+                .forEach(s -> System.out.println());
+
+        collect.forEach(System.out::println);
+
+        list.stream()
+                .filter(s -> s.length() > 3)
+                .filter(s -> s.startsWith("F") || s.startsWith("W"))
+                .map(Strumienie::addSurname)
+                .forEach(System.out::println);
     }
 
+    private static String addSurname(String s) {
+        if (s.endsWith("a")) {
+            return s + " Kowalska";
+        } else {
+            return s + " Kowalski";
+        }
+    }
 }
